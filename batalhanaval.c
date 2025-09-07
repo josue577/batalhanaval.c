e4f32b032e1f57effb37b110c49b055c3260df1c
josue577 <jojoferreira40@gmail.com>
2025-09-07 23:19:40 +0000

    desafio difícil d3

--- batalhanaval.c
+++ batalhanaval.c
@@ -10,6 +10,19 @@ int main(){
 
 int tabuleiro [tam][tam];
 
+// Configuração do cone (flexível)
+int linha_inicio_cone = 7;    // linha onde o cone começa
+int coluna_centro_cone = 2;   // coluna central do cone
+int altura_cone = 3;          // quantas linhas o cone ocupa
+
+// Configuração da cruz
+int centro_linha_cruz = 8;
+int centro_coluna_cruz = 7;
+
+// Configuração do octaedro
+int linha_centro_octa = 1;
+int coluna_centro_octa = 1;
+
 for (int i = 0; i < tam; i++)
 {
     for (int j = 0; j < tam; j++)
@@ -17,6 +30,27 @@ for (int i = 0; i < tam; i++)
         tabuleiro[i][j] = agua;
     }
     
+}
+
+for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+        if (i >= linha_inicio_cone && i < linha_inicio_cone + altura_cone &&
+            j >= coluna_centro_cone - (i - linha_inicio_cone) &&
+            j <= coluna_centro_cone + (i - linha_inicio_cone)) {
+            tabuleiro[i][j] = 1;
+        }
+
+        if ((i >= centro_linha_cruz - 1 && i <= centro_linha_cruz + 1 && j == centro_coluna_cruz) ||  
+            (i == centro_linha_cruz && j >= centro_coluna_cruz - 2 && j <= centro_coluna_cruz + 2)) {  
+            tabuleiro[i][j] = 1;
+        }
+
+        if ((i == linha_centro_octa - 1 && j == coluna_centro_octa) ||       // topo
+                (i == linha_centro_octa && j >= coluna_centro_octa - 1 && j <= coluna_centro_octa + 1) || // meio
+                (i == linha_centro_octa + 1 && j == coluna_centro_octa)) {       // base
+                tabuleiro[i][j] = 1;
+            }
+    }
 }
 
 for (int i = 3; i < 6; i++)
@@ -34,7 +68,7 @@ for (int i = 3; i < 6; i++)
     tabuleiro[i][9-i] = navio;
 }
 
-for (int i = 6; i < 9; i++)
+for (int i = 2; i < 5; i++)
 {
     tabuleiro[i][i] = navio;
 }
--- texte.c
+++ texte.c
@@ -3,12 +3,25 @@
 #define tam 10
 #define navio 3
 #define agua 0
-
+#define hab 1
 
 
 int main(){
 
 int tabuleiro [tam][tam];
+
+// Configuração do cone (flexível)
+int linha_inicio_cone = 7;    // linha onde o cone começa
+int coluna_centro_cone = 2;   // coluna central do cone
+int altura_cone = 3;          // quantas linhas o cone ocupa
+
+// Configuração da cruz
+int centro_linha_cruz = 8;
+int centro_coluna_cruz = 7;
+
+// Configuração do octaedro
+int linha_centro_octa = 1;
+int coluna_centro_octa = 1;
 
 for (int i = 0; i < tam; i++)
 {
@@ -19,33 +32,36 @@ for (int i = 0; i < tam; i++)
     
 }
 
-for (int i = 3; i < 6; i++)
-{
-    tabuleiro[2][i] = navio;
+for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+        if (i >= linha_inicio_cone && i < linha_inicio_cone + altura_cone &&
+            j >= coluna_centro_cone - (i - linha_inicio_cone) &&
+            j <= coluna_centro_cone + (i - linha_inicio_cone)) {
+            tabuleiro[i][j] = 1;
+        }
+
+        if ((i >= centro_linha_cruz - 1 && i <= centro_linha_cruz + 1 && j == centro_coluna_cruz) ||  
+            (i == centro_linha_cruz && j >= centro_coluna_cruz - 2 && j <= centro_coluna_cruz + 2)) {  
+            tabuleiro[i][j] = 1;
+        }
+
+        if ((i == linha_centro_octa - 1 && j == coluna_centro_octa) ||       // topo
+                (i == linha_centro_octa && j >= coluna_centro_octa - 1 && j <= coluna_centro_octa + 1) || // meio
+                (i == linha_centro_octa + 1 && j == coluna_centro_octa)) {       // base
+                tabuleiro[i][j] = 1;
+            }
+    }
 }
 
-for (int i = 4; i < 7; i++)
-{
-    tabuleiro[i][2] = navio;
-}
-
-for (int i = 3; i < 6; i++)
-{
-    tabuleiro[i][9-i] = navio;
-}
-
-for (int i = 6; i < 9; i++)
-{
-    tabuleiro[i][i] = navio;
-}
 
 
-printf ("     A B C D E F G H I J\n\n");
+
+printf ("   A B C D E F G H I J\n");
 int numero = 0;
 for (int i = 0; i < tam; i++)
 {
     numero++;
-    printf ("%2d   ", numero);
+    printf ("%2d ", numero);
     for (int j = 0; j < tam; j++)
     {
         printf ("%d ", tabuleiro[i][j]);
